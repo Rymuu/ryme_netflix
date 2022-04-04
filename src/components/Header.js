8335efc75397f70d83b3451bb1b29c6b8a5403e4
Rymuu <sutrils-nozu@outlook.fr>
2022-04-04 17:21:13 +0200

    Modification Header/MyList

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,54 @@ import React, { useEffect } from "react";
 import Link from "next/link";
 import LogoNetflix from "../public/netflix.png";
 import Button from "./Button";
+import { useRouter } from "next/router";
+
 const Header = () => {
+    const router = useRouter();
   return (
-    <header className="header__main">
-      <div className="header__logo">
-        <img src={LogoNetflix.src} alt="logo_netflix" />
-      </div>
-      <nav className="header__nav">
-        <ul className="nav__list">
-          <li className="nav__item">
-          <Button title="S'identifier" classes="btn btn__color-red__shape-rounded" type="submit"/>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <>
+      {router.asPath === "/" ?
+
+        (
+          <header className="header__main">
+            <div className="header__logo">
+              <img src={LogoNetflix.src} alt="logo_netflix" />
+            </div>
+            <nav className="header__nav">
+              <ul className="nav__list">
+                <li className="nav__item">
+                  <Button title="S'identifier" classes="btn btn__color-red__shape-rounded" type="submit" />
+                </li>
+              </ul>
+            </nav>
+          </header>
+        )
+        :
+        (
+          <header className="header__main">
+            <div className="header__logo">
+              <img src={LogoNetflix.src} alt="logo_netflix" />
+            </div>
+            <nav className="header__nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link href="/browse">
+                  <a className="nav__link">Accueil</a>
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link href="/my-list">
+                  <a className="nav__link">Ma liste</a>
+                </Link>
+              </li>
+            </ul>
+            </nav>
+          </header>
+        )
+      }
+    </>
+
+
   );
 };
 
